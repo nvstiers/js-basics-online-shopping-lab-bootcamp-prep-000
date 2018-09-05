643bbb1bb14f2b8d43267c5f8f3b98e32dc825df
Nathaniel Stiers <nvstiers@gmail.com>
2018-09-05 06:38:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,9 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    var cartTotal = total()
+    cart.length = 0
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
   }
-  cart.length = 0
+
 }
